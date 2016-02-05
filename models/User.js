d16e5ccc0978d629b47667e0b472bafa8b8d6280
aadesh <aadeshp95@gmail.com>
2016-02-05 12:23:51 -0500

    fixes #6
    Removed passwordHash key/value from user JSON

--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
-var userSchema = new mongoose.Schema({
+var UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     passwordHash: { type: String, required: true } 
 });
 
-module.exports = mongoose.model("User", userSchema);
+UserSchema.methods.toJSON = function() {
+    var obj = this.toObject();
+    delete obj.passwordHash;
+    return obj;
+};
+
+module.exports = mongoose.model("User", UserSchema);

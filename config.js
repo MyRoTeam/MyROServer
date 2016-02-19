1879cfbd86b7acbf93d44e6b965a932e9f845b47
aadesh <aadeshp95@gmail.com>
2016-02-18 23:49:08 -0500

    added config.js file that contains a dictionary of const values

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'database': 'mongodb://localhost/ngb_db',
+    'userTokenSecret': 'svGOM%oeLNH$NJm.hk9TIW$H9ZP!{hl<',
+    'robotTokenSecret': 'k&uJjW*zn0l7SLB]6>Rg.){GB5Z(I?C;'
+};

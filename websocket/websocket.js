f5989e4e2f93a33f3c59c68ac9147dcb931dbfd2
aadesh <aadeshp95@gmail.com>
2016-03-01 14:24:26 -0500

    fixed bug with jwt where it was retrieving a malformed object

--- websocket/websocket.js
+++ websocket/websocket.js
@@ -1,5 +1,10 @@
 const config = require('../config');
 
+/*
+ * Websocket broker between client and robot,
+ * which is primarily used to send instructions
+ * from the client to a specific robot
+ */
 function WebSocket(http) {
     this.io = require('socket.io')(http);
     this.ioJwt = require('socketio-jwt');
@@ -8,28 +13,37 @@ function WebSocket(http) {
         console.log('User Disconnected from WebSocket');
     };
 
-    this.onReceive = function(msg) {
-        console.log("Received Message: " + msg);
-
-        this.io.emit('instruction', msg);
-    };
-
     this.onReceive = function(udid) {
         return function(msg) {
             console.log("Received Message: " + msg);
+
+            /* Sends the message to the robot who has the supplied UDID */
             this.io.emit('instruction-' + udid, msg);
         };
     };
 
+    /*
+     * Runs on websocket initial connection only if the supplied
+     * robot token was valid
+     */
     this.onAuthenticated = function(socket) {
         socket.on('disconnect', this.onDisconnect);
-        socket.on('instruction', this.onReceive(socket.decoded_token));
+
+        /* 
+         * decoded_token.udid is the robot's unique UDID which was used
+         * to generate the token
+         */
+        socket.on('instruction', this.onReceive(socket.decoded_token.udid));
     };
 
-    this.io.on('connection', this.ioJwt.authorize({
-                secret: config.robotTokenSecret
-            }))
-           .on('authenticated', this.onAuthenticated);
+    /*
+     * Authenticate supplied robot token that the user retrieved
+     * from calling the /users/:id/connect api route. Websocket
+     * connection will only be successful if the robot token
+     * is successfully verified
+     */
+    this.io.on('connection', this.ioJwt.authorize({ secret: config.robotTokenSecret }))
+        .on('authenticated', this.onAuthenticated);
 
 };
 
